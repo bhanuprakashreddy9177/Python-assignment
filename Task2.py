45b1ca5fba929fde16882e5ffcf8e3e6fc565f3e
bhanuprakashreddy9177 <bhanuprakashreddypatlolla111@gmail.com>
2025-07-04 21:50:53 +0530

    Task2.py

--- Task2.py
+++ Task2.py
@@ -4,4 +4,4 @@
 #Taking First name and the last name as two inputs from the user
 f1= input('Enter Your first Name: ')
 f2= input('Enter Your Second Name: ')
-print("Hello, " + f1+ f2+ " Welcome to the python program")
+print("Hello, " + f1+ " "+ f2+ " Welcome to the python program")
