daebca1b97758a13048e69bd47fb333eb33a486e
bhanuprakashreddy9177 <bhanuprakashreddypatlolla111@gmail.com>
2025-07-04 19:15:14 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,19 @@
+#Task1
+#Performing basic mathematical operations
+#Taking two numbers as input from the user
+
+x= float(input('Enter the first number: '))
+y= float(input('Enter the second number: '))
+
+#performing operations with the given inputs
+print("Addition: " ,x+y)
+print("Subtraction: ", x-y)
+print("Multiplication: ", x*y)
+print("Division: ", x/y)
+
+
+
+
+
+
+
---
+++ Task2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Task2
+# Creating a Personalized Greeting
+
+#Taking First name and the last name as two inputs from the user
+f1= input('Enter Your first Name: ')
+f2= input('Enter Your Second Name: ')
+print("Hello, " + f1+ f2+ " Welcome to the python program")
+
+
+# In[ ]:
+
+
+
+
